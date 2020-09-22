9a16a5f79aa2b3b9c199543a58e4c5967cb7cfb2
Joey Sheridan <36169085+Agamago@users.noreply.github.com>
2020-09-22 14:23:46 -0700

    Create filecpy.c

---
+++ filecpy.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+//Allows for multiple arguments to be passed and stored in argv[]
+int main(int argc, char *argv[])
+{
+   //File pointers from passed arguments in command line
+   FILE *fptr1;
+   FILE *fptr2;
+
+   //Program variables
+   int i;
+   char ch;
+
+   //Test for argument functionality
+   printf("Total number of argument passed : %d\n", argc);
+
+   //Opens source file using argv[1] in read mode using "r" to check if it exists
+   if((fptr1 = fopen(argv[1], "r") ) == NULL)
+   {
+
+      printf("Error. . . \n The file cant be opened. . .\n");
+      return -1;
+
+   }
+
+   //Opens the destination file
+   fptr2 = fopen(argv[2], "w");
+
+   //While the end of the file has not yet been reached , copy the next character to variable ch
+   //then, put ch into ptr2(destination) with putc
+   while( !feof(fptr1))
+   {
+      ch = getc(fptr1);
+      if (ch != EOF)
+      {
+         putc(ch,fptr2);
+      }
+   }
+
+   //Close connections
+   fclose(fptr1);
+   fclose(fptr2);
+
+   printf("File copy complete");
+
+   return 0;
+
+}
