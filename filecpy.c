e275c12c527004c4a3af30c2152474bed0098a87
Joey <csheridan@chapman.edu>
2020-09-22 15:38:06 -0700

    done

--- filecpy.c
+++ filecpy.c
@@ -7,7 +7,7 @@
  *
  * To run, compile filecpu() with gcc and then run the following command :
  * ./a.out source.txt destination.txt
- *
+ 
  */
 
 
@@ -19,8 +19,8 @@
 int main(int argc, char *argv[])
 {
    //File pointers from passed arguments in command line
-   FILE *fptr1;
-   FILE *fptr2;
+   FILE *source;
+   FILE *destination;
 
    //Program variables
    int i;
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
    printf("Total number of argument passed : %d\n", argc);
 
    //Opens source file using argv[1] in read mode using "r" to check if it exists
-   if((fptr1 = fopen(argv[1], "r") ) == NULL)
+   if((source = fopen(argv[1], "r") ) == NULL)
    {
 
       printf("Error. . . \n The file cant be opened. . .\n");
@@ -39,22 +39,22 @@ int main(int argc, char *argv[])
    }
 
    //Opens the destination file
-   fptr2 = fopen(argv[2], "w");
+   destination = fopen(argv[2], "w");
 
    //While the end of the file has not yet been reached , copy the next character to variable ch
    //then, put ch into ptr2(destination) with putc
-   while( !feof(fptr1))
+   while( !feof(source))
    {
-      ch = getc(fptr1);
+      ch = getc(source);
       if (ch != EOF)
       {
-         putc(ch,fptr2);
+         putc(ch,destination);
       }
    }
 
    //Close connections
-   fclose(fptr1);
-   fclose(fptr2);
+   fclose(source);
+   fclose(destination);
 
    printf("File copy complete");
 
